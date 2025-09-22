6029aa58b8ee53f86fb913582d74c0e4b4182ad2
Manuel Spagnolo <manuel.spagnolo@proton.me>
2025-09-22 21:51:40 +0200

    feat(test.h): add FAILED_ONLY and few more switches to test.h

--- lib/test.h
+++ lib/test.h
@@ -1,10 +1,12 @@
-// Test (v0.0.1)
+// Test (v0.1.0)
 // ---
 //
 // ## Getting Started
 //
-// Compile the test executable returning `test__report()` to get the number of
+// Compile the test executable returning `report()` to get the number of
 // failed tests as the status code.
+//
+// Define FAILED_ONLY to see only failures.
 //
 // ```c
 // // example.test.c
@@ -12,12 +14,12 @@
 // #include "../test/test.h"
 //
 // void testExample() {
-//   test__expectTrue(example(), "returns true");
+//   expectTrue(example(), "returns true");
 // }
 //
 // int main(void) {
-//   test__suite(testExample);
-//   return test__report();
+//   suite(testExample);
+//   return report();
 // }
 // ```
 //
@@ -28,21 +30,21 @@
 // #include "my_struct.h" // myStruct_eql, myStruct_toString
 // #include "../test/test.h"
 //
-// void expectEqlMyStruct(const MyStruct* a, const MyStruct* b, const char* name) {
+// void expectEqlMyType(const MyType* a, const MyType* b, const char* name) {
 //   char msg[256];
-//   snprintf(msg, 256, "Expected %s to equal %s", myStruct_toString(a), myStruct_toString(b));
-//   test__expect(myStruct_eql(a, b), name, msg);
+//   snprintf(msg, 256, "Expected %s to equal %s", myStruct_toString(a),
+//   myStruct_toString(b));
+//   expect(myStruct_eql(a, b), name, msg);
 // }
 //
 // // define main as above
 // ```
 // ___HEADER_END___
 
-#ifndef TEST_H
-#define TEST_H
+#pragma once
 
-#include <stdio.h>  // printf, snprintf
 #include <math.h>   // fabs
+#include <stdio.h>  // printf, snprintf
 #include <string.h> // strncmp
 
 #define FLOAT_THRESHOOLD 1e-6f
@@ -52,13 +54,14 @@ static int total = 0;
 static int failed = 0;
 
 /**
-  * Asserts a condition and prints the result. Use it to immplement custom assertions.
-  * @name expect
-  * @example
-  *   int a = 1;
-  *   test__expect(a == 1, "a is one", "Should be true");
-  */
-void test__expect(bool condition, const char* name, const char* message) {
+ * Asserts a condition and prints the result. Use it to implement custom
+ * assertions.
+ * @name expect
+ * @example
+ *   int a = 1;
+ *   expect(a == 1, "a is one", "Should be true");
+ */
+void expect(bool condition, const char *name, const char *message) {
   total++;
   if (!condition) {
     failed++;
@@ -69,171 +72,247 @@ void test__expect(bool condition, const char* name, const char* message) {
 }
 
 /**
-  * Asserts that a condition is true.
-  * @name expectTrue
-  * @example
-  *   test__expectTrue(2 > 1, "2 is greater than 1");
-  */
-void test__expectTrue(bool condition, const char* name) {
-  test__expect(condition, name, "Expected value to be true");
+ * Asserts that a condition is true.
+ * @name expectTrue
+ * @example
+ *   expectTrue(2 > 1, "2 is greater than 1");
+ */
+void expectTrue(bool condition, const char *name) {
+  expect(condition, name, "Expected value to be true");
 }
 
 /**
-  * Asserts that a condition is false.
-  * @name expectFalse
-  * @example
-  *   test__expectFalse(0, "zero is false");
-  */
-void test__expectFalse(bool condition, const char* name) {
-  test__expect(!condition, name, "Expected value to be false");
+ * Asserts that a condition is false.
+ * @name expectFalse
+ * @example
+ *   expectFalse(0, "zero is false");
+ */
+void expectFalse(bool condition, const char *name) {
+  expect(!condition, name, "Expected value to be false");
 }
 
 /**
-  * Asserts that a pointer is not NULL.
-  * @name expectNotNull
-  * @example
-  *   int x = 5;
-  *   test__expectNotNull(&x, "pointer is not null");
-  */
-void test__expectNotNull(const void* a, const char* name) {
-  test__expect(a != NULL, name, "Expected value not to be null");
+ * Asserts that a pointer is not NULL.
+ * @name expectNotNull
+ * @example
+ *   int x = 5;
+ *   expectNotNull(&x, "pointer is not null");
+ */
+void expectNotNull(const void *a, const char *name) {
+  expect(a != NULL, name, "Expected value not to be null");
 }
 
 /**
-  * Asserts that a pointer is NULL.
-  * @name expectNull
-  * @example
-  *   int* p = NULL;
-  *   test__expectNull(p, "pointer is null");
-  */
-void test__expectNull(const void* a, const char* name) {
-  test__expect(a == NULL, name, "Expected value to be null");
+ * Asserts that a pointer is NULL.
+ * @name expectNull
+ * @example
+ *   int* p = NULL;
+ *   expectNull(p, "pointer is null");
+ */
+void expectNull(const void *a, const char *name) {
+  expect(a == NULL, name, "Expected value to be null");
 }
 
 /**
-  * Asserts that two integers are equal.
-  * @name expectEqlInt
-  * @example
-  *   test__expectEqlInt(3, 3, "3 equals 3");
-  */
-void test__expectEqlInt(const int a, const int b, const char* name) {
+ * Asserts that two integers are equal.
+ * @name expectEqli
+ * @example
+ *   expectEqli(3, 3, "3 equals 3");
+ */
+void expectEqli(const int a, const int b, const char *name) {
   char msg[256];
   snprintf(msg, 256, "Expected %d to equal %d", a, b);
-  test__expect(a == b, name, msg);
+  expect(a == b, name, msg);
 }
 
 /**
-  * Asserts that two integers are not equal.
-  * @name expectNeqInt
-  * @example
-  *   test__expectNeqInt(3, 4, "3 does not equal 4");
-  */
-void test__expectNeqInt(const int a, const int b, const char* name) {
+ * Asserts that two integers are not equal.
+ * @name expectNeqi
+ * @example
+ *   expectNeqi(3, 4, "3 does not equal 4");
+ */
+void expectNeqi(const int a, const int b, const char *name) {
   char msg[256];
   snprintf(msg, 256, "Expected %d not to equal %d", a, b);
-  test__expect(a != b, name, msg);
+  expect(a != b, name, msg);
 }
 
 /**
-  * Asserts that two floats are equal within a threshold.
-  * @name expectEqlFloat
-  * @example
-  *   test__expectEqlFloat(1.0f, 1.0f, "floats are equal");
-  */
-void test__expectEqlFloat(const float a, const float b, const char* name) {
+ * Asserts that two unsigned integers are equal.
+ * @name expectEqlu
+ * @example
+ *   expectEqlu(3, 3, "3 equals 3");
+ */
+void expectEqlu(const unsigned int a, const unsigned int b, const char *name) {
+  char msg[256];
+  snprintf(msg, 256, "Expected %u to equal %u", a, b);
+  expect(a == b, name, msg);
+}
+
+/**
+ * Asserts that two unsigned integers are not equal.
+ * @name expectNeqlu
+ * @example
+ *   expectNeqlu(3, 4, "3 does not equal 4");
+ */
+void expectNeqlu(const unsigned int a, const unsigned int b, const char *name) {
+  char msg[256];
+  snprintf(msg, 256, "Expected %u not to equal %u", a, b);
+  expect(a != b, name, msg);
+}
+
+/**
+ * Asserts that two unsigned long integers are equal.
+ * @name expectEqllu
+ * @example
+ *   expectEqllu(3, 3, "3 equals 3");
+ */
+void expectEqllu(const size_t a, const size_t b, const char *name) {
+  char msg[256];
+  snprintf(msg, 256, "Expected %lu to equal %lu", a, b);
+  expect(a == b, name, msg);
+}
+
+/**
+ * Asserts that two unsigned long integers are equal.
+ * @name expectNeqllu
+ * @example
+ *   expectNeqllu(3, 4, "3 does not equal 4");
+ */
+void expectNeqllu(const size_t a, const size_t b, const char *name) {
+  char msg[256];
+  snprintf(msg, 256, "Expected %lu not to equal %lu", a, b);
+  expect(a != b, name, msg);
+}
+
+/**
+ * Asserts that two floats are equal within a threshold.
+ * @name expectEqlf
+ * @example
+ *   expectEqlf(1.0f, 1.0f, "floats are equal");
+ */
+void expectEqlf(const float a, const float b, const char *name) {
   char msg[256];
   snprintf(msg, 256, "Expected %f to equal %f", a, b);
-  test__expect(fabsf(a - b) < FLOAT_THRESHOOLD, name, msg);
+  expect(fabsf(a - b) < FLOAT_THRESHOOLD, name, msg);
 }
 
 /**
-  * Asserts that two floats are not equal within a threshold.
-  * @name expectNeqFloat
-  * @example
-  *   test__expectNeqFloat(1.0f, 2.0f, "floats are not equal");
-  */
-void test__expectNeqFloat(const float a, const float b, const char* name) {
+ * Asserts that two floats are not equal within a threshold.
+ * @name expectNeqf
+ * @example
+ *   expectNeqf(1.0f, 2.0f, "floats are not equal");
+ */
+void expectNeqf(const float a, const float b, const char *name) {
   char msg[256];
   snprintf(msg, 256, "Expected %f not to equal %f", a, b);
-  test__expect(fabsf(a - b) >= FLOAT_THRESHOOLD, name, msg);
+  expect(fabsf(a - b) >= FLOAT_THRESHOOLD, name, msg);
 }
 
 /**
-  * Asserts that two doubles are equal within a threshold.
-  * @name expectEqlDouble
-  * @example
-  *   test__expectEqlDouble(1.0, 1.0, "doubles are equal");
-  */
-void test__expectEqlDouble(const double a, const double b, const char* name) {
+ * Asserts that two doubles are equal within a threshold.
+ * @name expectEqld
+ * @example
+ *   expectEqld(1.0, 1.0, "doubles are equal");
+ */
+void expectEqld(const double a, const double b, const char *name) {
   char msg[256];
   snprintf(msg, 256, "Expected %f to equal %f", a, b);
-  test__expect(fabs(a - b) < DOUBLE_THRESHOOLD, name, msg);
+  expect(fabs(a - b) < DOUBLE_THRESHOOLD, name, msg);
 }
 
 /**
-  * Asserts that two doubles are not equal within a threshold.
-  * @name expectNeqDouble
-  * @example
-  *   test__expectNeqDouble(1.0, 2.0, "doubles are not equal");
-  */
-void test__expectNeqDouble(const double a, const double b, const char* name) {
+ * Asserts that two doubles are not equal within a threshold.
+ * @name expectNeqd
+ * @example
+ *   expectNeqd(1.0, 2.0, "doubles are not equal");
+ */
+void expectNeqd(const double a, const double b, const char *name) {
   char msg[256];
   snprintf(msg, 256, "Expected %f not to equal %f", a, b);
-  test__expect(fabs(a - b) >= DOUBLE_THRESHOOLD, name, msg);
+  expect(fabs(a - b) >= DOUBLE_THRESHOOLD, name, msg);
 }
 
 /**
-  * Asserts that two strings are equal up to max_size.
-  * @name expectEqlString
-  * @example
-  *   test__expectEqlString("foo", "foo", 3, "strings are equal");
-  */
-void test__expectEqlString(const char* a, const char* b, size_t max_size, const char* name) {
+ * Asserts that two strings are equal up to max_size.
+ * @name expectEqls
+ * @example
+ *   expectEqls("foo", "foo", 3, "strings are equal");
+ */
+void expectEqls(const char *a, const char *b, size_t max_size,
+                const char *name) {
   char msg[256];
   snprintf(msg, 256, "Expected '%s' to equal '%s'", a, b);
-  test__expect(strncmp(a, b, max_size) == 0, name, msg);
+  expect(strncmp(a, b, max_size) == 0, name, msg);
 }
 
 /**
-  * Asserts that two strings are not equal up to max_size.
-  * @name expectNeqString
-  * @example
-  *   test__expectNeqString("foo", "bar", 3, "strings are not equal");
-  */
-void test__expectNeqString(const char* a, const char* b, size_t max_size, const char* name) {
+ * Asserts that two strings are not equal up to max_size.
+ * @name expectNeqs
+ * @example
+ *   expectNeqs("foo", "bar", 3, "strings are not equal");
+ */
+void expectNeqs(const char *a, const char *b, size_t max_size,
+                const char *name) {
   char msg[256];
   snprintf(msg, 256, "Expected '%s' not to equal '%s'", a, b);
-  test__expect(strncmp(a, b, max_size) != 0, name, msg);
+  expect(strncmp(a, b, max_size) != 0, name, msg);
 }
 
 /**
-  * Prints a summary of test results and returns the number of failed tests.
-  * @name report
-  * @example
-  *   return test__report();
-  */
-int test__report(void) {
+ * Asserts that first string includes the second.
+ * @name expectIncls
+ * @example
+ *   expectIncls("foobar", "bar", "strings are included");
+ */
+void expectIncls(const char *big, const char *small, const char *name) {
+  char msg[256];
+  snprintf(msg, 256, "Expected '%s' to include '%s'", big, small);
+  expect(strstr(big, small) != NULL, name, msg);
+}
+
+/**
+ * Prints a summary of test results and returns the number of failed tests.
+ * @name report
+ * @example
+ *   return report();
+ */
+int report(void) {
+#ifndef FAILED_ONLY
   printf("\n%d assertions, %d failed\n", total, failed);
+#else
+  printf("%d assertions, %d failed\n", total, failed);
+#endif
   return failed;
 }
 
 /**
-  * Prints the name of a test case.
-  * @name report
-  * @example
-  *    test__case("my test");
-  */
-#define test__case(name) printf("  %s:\n", name)
+ * Prints the name of a test case.
+ * @name case
+ * @example
+ *    case("my test");
+ */
+#ifndef FAILED_ONLY
+#define case(name) printf("  %s:\n", name)
+#else
+#define case(name)
+#endif
 
 /**
-  * Runs a test suite and prints its name.
-  * @name suite
-  * @example
-  *    test__suite(myTestFunction);
-  */
-#define test__suite(name) {\
-  printf("\n> %s\n", #name);\
-  name();\
-}
-
-#endif //TEST_H
+ * Runs a test suite and prints its name.
+ * @name suite
+ * @example
+ *    suite(myTestFunction);
+ */
+#ifndef FAILED_ONLY
+#define suite(name)                                                            \
+  {                                                                            \
+    printf("\n> %s\n", #name);                                                 \
+    name();                                                                    \
+  }
+#else
+#define suite(name)                                                            \
+  {                                                                            \
+    name();                                                                    \
+  }
+#endif
